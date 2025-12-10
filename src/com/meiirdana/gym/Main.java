32a56f8a1e14de9bccd6b712fdb53f7755e4a6fd
javastudent2502 <250287@astanait.edu.kz>
2025-12-10 10:13:33 +0500

    Initial commit: first Java project

---
+++ src/com/meiirdana/gym/Main.java
@@ -0,0 +1,15 @@
+package com.meiirdana.gym;
+
+public class Main {
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to Hotel Management System!");
+            System.out.println("This system is created to manage hotel operations.");
+            System.out.println("Main features:");
+            System.out.println("- Room booking");
+            System.out.println("- Customer management");
+            System.out.println("- Payment processing");
+            System.out.println("Developer: Mika, OOP Course");
+
+        }
+    }
