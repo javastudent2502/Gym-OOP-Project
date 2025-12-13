3144d98c734af62d2a949b18ad61ebe551c86163
javastudent2502 <250287@astanait.edu.kz>
2025-12-13 18:10:58 +0500

    Commit

--- src/com/meiirdana/gym/Main.java
+++ src/com/meiirdana/gym/Main.java
@@ -3,13 +3,13 @@ package com.meiirdana.gym;
 public class Main {
         public static void main(String[] args) {
 
-            System.out.println("Welcome to Hotel Management System!");
-            System.out.println("This system is created to manage hotel operations.");
+            System.out.println("Welcome to Gym Management System!");
+            System.out.println("This system is created to manage gym operations.");
             System.out.println("Main features:");
-            System.out.println("- Room booking");
+            System.out.println("-Fitness classes");
             System.out.println("- Customer management");
             System.out.println("- Payment processing");
-            System.out.println("Developer: Mika, OOP Course");
+            System.out.println("Developer: Meiirdana, OOP Course");
 
         }
     }
